31acf9ba65a7f118734034cfc3d60c8c81607fc3
douglasms <douglasms90@gmail.com>
2022-12-17 13:23:48 -0300

    building the first factory

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
-import discord
+import discord, os
 from discord.ext.commands import Bot, Context
-from ext.webscraping import init_api
+from ext import webscraping
+from dotenv import load_dotenv
 
+
+load_dotenv()
+token = os.getenv('BotToken')
 
 client = Bot(command_prefix='.', intents=discord.Intents.all())
 
@@ -9,10 +13,10 @@ client = Bot(command_prefix='.', intents=discord.Intents.all())
 async def test(contex):
     await contex.send("Hi!")
 
-
 @client.command()
 async def series(contex):
-    for serie in init_api(links):
+    await contex.send("**Aguarde um momento**...")
+    for serie in webscraping.init_api():
         await contex.send(serie)
 
-client.run('')
+client.run(token)
--- ext/webscraping.py
+++ ext/webscraping.py
@@ -2,26 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def scraping(link):
-    return BeautifulSoup(requests.get(link).content, "html.parser")
+def scraping(source):
+    return BeautifulSoup(requests.get(source).content, "html.parser")
 
 def lacraiatorrent():
     data = [
-        web(id=1, name="Teles of the Walking Dead", season="s01", link="https://lacraiatorrent.com/tales-of-the-walking-dead-1-temporada-legendada-torrent/"),
-        web(id=2, name="Titans", season="s04", link="https://lacraiatorrent.com/titas-4-temporada-legendada-torrent/"),
-        web(id=3, name="Fear in the Walking Dead", season="s08", link="https://lacraiascraping.com/fear-the-walking-dead-8-temporada-torrent-dublada/"),
-        web(id=4, name="The Rings of Power", season="s02", link="https://lacraiascraping.com/o-senhor-dos-aneis-os-aneis-de-poder-2-temporada-torrent-dublada/"),
-        web(id=5, name="House of the Dragon", season="s02", link="https://lacraiascraping.com/a-casa-do-dragao-2-temporada-torrent-dublada/"),
-        web(id=6, name="Succession", season="s04", link="https://lacraiascraping.com/succession-4-temporada-torrent-dublada/"),
-        web(id=7, name="Barbarians", season="s03", link="https://lacraiascraping.com/barbaros-3-temporada-completa-torrent-dublada/"),
-        web(id=8, name="Arcanjo Renegado", season="s03", link="https://lacraiascraping.com/arcanjo-renegado-3-temporada-torrent/"),
-        web(id=9, name="She Hulk", season="s02", link="https://lacraiascraping.com/mulher-hulk-defensora-de-herois-2-temporada-torrent-dublada/"),
-        web(id=10, name="Sandman", season="s02", link="https://lacraiascraping.com/sandman-2-temporada-completa-torrent-dublada/")
+        {'name':'Teles of the Walking Dead', 'season':'s01', 'link':'https://lacraiatorrent.com/tales-of-the-walking-dead-1-temporada-legendada-torrent/'},
+        {'name':'Titans', 'season':'s04', 'link':'https://lacraiatorrent.com/titas-4-temporada-legendada-torrent/'},
+        {'name':'Fear in the Walking Dead', 'season':'s08', 'link':'https://lacraiatorrent.com/fear-the-walking-dead-8-temporada-torrent-dublada/'},
+        {'name':'The Rings of Power', 'season':'s02', 'link':'https://lacraiatorrent.com/o-senhor-dos-aneis-os-aneis-de-poder-2-temporada-torrent-dublada/'},
+        {'name':'House of the Dragon', 'season':'s02', 'link':'https://lacraiatorrent.com/a-casa-do-dragao-2-temporada-torrent-dublada/'},
+        {'name':'Succession', 'season':'s04', 'link':'https://lacraiatorrent.com/succession-4-temporada-torrent-dublada/'},
+        {'name':'Barbarians', 'season':'s03', 'link':'https://lacraiatorrent.com/barbaros-3-temporada-completa-torrent-dublada/'},
+        {'name':'Arcanjo Renegado', 'season':'s03', 'link':'https://lacraiatorrent.com/arcanjo-renegado-3-temporada-torrent/'},
+        {'name':'She Hulk', 'season':'s02', 'link':'https://lacraiatorrent.com/mulher-hulk-defensora-de-herois-2-temporada-torrent-dublada/'},
+        {'name':'Sandman', 'season':'s02', 'link':'https://lacraiatorrent.com/sandman-2-temporada-completa-torrent-dublada/'},
+        {'name':'See', 'season':'s04', 'link':'https://lacraiatorrent.com/pagina-nao-encontrada-torrent/'}
     ]
-    return data
 
-def init_api(links):
-    for link in links:
-        series = scraping(link).find_all("span", class_="botao_dublado")[-1].text
-    print(series)
+    series = list()
+    for i in data:
+        try:
+            series.append(f"{i['name']} {i['season']} {scraping(i['link']).find_all('span', class_='botao_dublado')[-1].text}")
+        except:
+            series.append(f"{i['name']} {i['season']} 'Aguardando dublagem'")
     return series
+
+def init_api():
+    result = lacraiatorrent()
+    return result
