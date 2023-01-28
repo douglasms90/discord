1dd63ad4c478d7819bbe7f50c3bceb10b4d04392
douglasms <douglasms90@gmail.com>
2023-01-28 16:29:16 -0300

    -mk

--- testdgbot/ext/database.py
+++ testdgbot/ext/database.py
@@ -13,23 +13,3 @@ engine = db.create_engine('sqlite:///development.db', echo=True)
 Session = dbo.sessionmaker(bind=engine)
 
 session = Session()
-<<<<<<< HEAD
-=======
-
-class dbConn():
-    conn = None
-    def __init__(self):
-        self.conn = psycopg2.connect("")
-
-    def consult(self, query):
-        try:
-            cur = self.conn.cursor()
-            cur.execute(query)
-            dbdump = cur.fetchall()
-            return dbdump
-        except:
-            return "Impossible to connect to the database, check your code."
-    
-    def close(self):
-        self.conn.close()
->>>>>>> 9f2c996b19c25bb968cefe62095ae5b10db00e78
