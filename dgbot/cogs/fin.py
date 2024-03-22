5d29c42b104fb5e9bd67eed541ea74d68c974b10
douglasms90 <douglasms90@gmail.com>
2024-03-22 09:57:11 -0300

    fin in disc

--- dgbot/cogs/fin.py
+++ dgbot/cogs/fin.py
@@ -7,6 +7,7 @@ from models import Atv
 from bs4 import BeautifulSoup
 import requests
 
+
 class atv(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -67,12 +68,11 @@ class atv(commands.Cog):
             yc += (dv/i.pm)*100
             tl += pr*i.qt
             ac += i.pm*i.qt
-            print(f"{i.id}  {i.nm.upper()}\t{'%.2f' %(((pr-i.pm)/i.pm)*100)}%\t{'%.0f' %(pr)}\t{'%.0f' %(i.pm)}\t{i.rc} {pa}\t{'%.0f' %(i.qt)}\t{dp}\t{yp}\t{pl}\t{vr}\t{'%.0f' %(pr*i.qt)}\t{'%.0f' %(i.pm*i.qt)}")
+            await ctx.send(f"`{i.id}\t{i.nm.upper()}\t{'%.2f' %(((pr-i.pm)/i.pm)*100)}%\t{'%.0f' %(pr)}\t{'%.0f' %(i.pm)}\t{i.rc}\t{pa}\t{'%.0f' %(i.qt)}\t{dp}\t{yp}\t{pl}\t{vr}\t{'%.0f' %(pr*i.qt)}\t{'%.0f' %(i.pm*i.qt)}`")
             dv = pl = vr = dp = yp = 0
-        print(f"rf: {'%.2f' %(tl*0.50)}\tfi: {'%.2f' %(tl*0.20)}\taç: {'%.2f' %(tl*0.15)}\tai: {'%.2f' %(tl*0.10)}\tcr: {'%.2f' %(tl*0.05)}")
-        print(f"rf: {'%.2f' %(arf)}\tfi: {'%.2f' %(afi)}\taç: {'%.2f' %(aab)}\tai: {'%.2f' %(aai)}\tcr: {'%.2f' %(acr)}\tdy: {'%.2f' %(dy/ct)}\trv: {'%.2f' %(arv)}\tac: {'%.2f' %(ac)}")
-        print(f"rf: {'%.2f' %(rf)}\tfi: {'%.2f' %(fi)}\taç: {'%.2f' %(ab)}\tai: {'%.2f' %(ai)}\tcr: {'%.2f' %(cr)}\tyc: {'%.2f' %(yc/ct)}\trv: {'%.2f' %(rv)}\ttl: {'%.2f' %(tl)}")
-        await ctx.send('Pronto!')
+        await ctx.send(f"`rf: {'%.2f' %(tl*0.50)}\tfi: {'%.2f' %(tl*0.20)}\taç: {'%.2f' %(tl*0.15)}\tai: {'%.2f' %(tl*0.10)}\tcr: {'%.2f' %(tl*0.05)}`")
+        await ctx.send(f"`rf: {'%.2f' %(arf)}\tfi: {'%.2f' %(afi)}\taç: {'%.2f' %(aab)}\tai: {'%.2f' %(aai)}\tcr: {'%.2f' %(acr)}\tdy: {'%.2f' %(dy/ct)}\trv: {'%.2f' %(arv)}\tac: {'%.2f' %(ac)}`")
+        await ctx.send(f"`rf: {'%.2f' %(rf)}\tfi: {'%.2f' %(fi)}\taç: {'%.2f' %(ab)}\tai: {'%.2f' %(ai)}\tcr: {'%.2f' %(cr)}\tyc: {'%.2f' %(yc/ct)}\trv: {'%.2f' %(rv)}\ttl: {'%.2f' %(tl)}`")
 
     @commands.command(name="atvinsert")
     async def insert(self, ctx, *args):
