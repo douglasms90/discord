304b6bf711f20f8d7a32c5ce55f33220cf70686c
douglasms90 <douglasms90@gmail.com>
2024-06-02 22:34:12 -0300

    music

--- dgbot/test/test.py
+++ dgbot/test/test.py
@@ -7,10 +7,9 @@ class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="play")
-    async def play(self, ctx, *args):
+    @commands.command()
+    async def play(ctx, url):
         # Configurações do youtube_dl
-        '''
         ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
@@ -19,20 +18,20 @@ class test(commands.Cog):
                 'preferredquality': '192',
             }],
         }
-        
+
         # Inicializa o youtube_dl
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
             url = info['formats'][0]['url']  # Obtém a URL do áudio
-        
+
         # Conecta ao canal de voz e reproduz o áudio
         voice_channel = ctx.author.voice.channel
         voice_client = await voice_channel.connect()
         voice_client.play(discord.FFmpegPCMAudio(url))
-        '''
-        channel = ctx.author.voice.channel.id
-        print(channel)
-        await channel.connect()
+
+    @commands.command(name="out")
+    async def out(self, ctx):
+        await ctx.voice_client.disconnect()
 
 async def setup(bot):
     await bot.add_cog(test(bot))
