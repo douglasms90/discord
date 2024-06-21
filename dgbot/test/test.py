9cd422e7aa705621abf5d5897900141e589837d7
douglasms90 <douglasms90@gmail.com>
2024-06-21 17:00:41 -0300

    db/test

--- dgbot/test/test.py
+++ dgbot/test/test.py
@@ -6,32 +6,38 @@ import youtube_dl
 class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    '''
     @commands.command()
-    async def play(ctx, url):
-        # Configurações do youtube_dl
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
+    async def join(ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect()
 
-        # Inicializa o youtube_dl
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url = info['formats'][0]['url']  # Obtém a URL do áudio
-
-        # Conecta ao canal de voz e reproduz o áudio
-        voice_channel = ctx.author.voice.channel
-        voice_client = await voice_channel.connect()
-        voice_client.play(discord.FFmpegPCMAudio(url))
-
-    @commands.command(name="out")
-    async def out(self, ctx):
+    # Comando para o bot sair do canal de voz
+    @commands.command()
+    async def leave(ctx):
         await ctx.voice_client.disconnect()
 
+    # Comando para tocar música do YouTube
+    @commands.command()
+    async def play(ctx, url):
+        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        vc = ctx.voice_client
+
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+
+    # Comando para pausar a música
+    @commands.command()
+    async def pause(ctx):
+        await ctx.voice_client.pause()
+
+    # Comando para continuar a música
+    @commands.command()
+    async def resume(ctx):
+        await ctx.voice_client.resume()
+    '''
 async def setup(bot):
     await bot.add_cog(test(bot))
