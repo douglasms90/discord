d5bf75fb09aa27c0600b426ffc68909dffc06e04
douglasms90 <douglasms90@gmail.com>
2024-07-03 18:17:28 -0300

    db

--- dgbot/test/test.py
+++ dgbot/test/test.py
@@ -7,37 +7,9 @@ class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     '''
-    @commands.command()
-    async def join(ctx):
-        channel = ctx.author.voice.channel
-        await channel.connect()
-
-    # Comando para o bot sair do canal de voz
-    @commands.command()
-    async def leave(ctx):
-        await ctx.voice_client.disconnect()
-
-    # Comando para tocar música do YouTube
-    @commands.command()
-    async def play(ctx, url):
-        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
-        vc = ctx.voice_client
-
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-            vc.play(source)
-
-    # Comando para pausar a música
-    @commands.command()
-    async def pause(ctx):
-        await ctx.voice_client.pause()
-
-    # Comando para continuar a música
-    @commands.command()
-    async def resume(ctx):
-        await ctx.voice_client.resume()
+    @commands.command(name='test')
+    async def test(self, ctxs):
+        await ctx.send('test')
     '''
 async def setup(bot):
     await bot.add_cog(test(bot))
