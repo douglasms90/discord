40baa12c9e9af8b58735e5e5518e0980d51abb25
douglasms90 <douglasms90@gmail.com>
2024-07-05 19:09:19 -0300

    test

--- dgbot/test/test.py
+++ dgbot/test/test.py
@@ -6,10 +6,10 @@ import youtube_dl
 class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    '''
-    @commands.command(name='test')
-    async def test(self, ctxs):
+
+    @commands.command(name='testando')
+    async def test(self, ctx):
         await ctx.send('test')
-    '''
+
 async def setup(bot):
     await bot.add_cog(test(bot))
