4978dc33a2c3e68ea5282dc15779dd76140946bd
douglasms90 <douglasms90@gmail.com>
2024-07-13 17:29:54 -0300

    db

--- dgbot/ext/database.py
+++ dgbot/ext/database.py
@@ -1,6 +1,11 @@
 import sqlalchemy
 import sqlalchemy.orm
 
+from psycopg2 import connect
+import psycopg2.extensions
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
 
 db = sqlalchemy
 
@@ -13,3 +18,23 @@ engine = db.create_engine('sqlite:///development.db', echo=True)
 Session = dbo.sessionmaker(bind=engine)
 
 session = Session()
+
+class dbc():
+    db_ = None
+    def __init__(self, host):
+        self.db_ = psycopg2.connect(host)
+
+    def consult(self, query):
+        try:
+            cur = self.db_.cursor()
+            cur.execute(query)
+            dump = cur.fetchall()
+            return dump
+        except:
+            return "Impossible to connect to the database, check your code."
+
+    def close(self):
+        self.db_.close()
+
+def init_app(app):
+    db.init_app(app)
--- dgbot/test/test.py
+++ dgbot/test/test.py
@@ -1,15 +1,27 @@
 import discord
 from discord.ext import commands
-import youtube_dl
+from decouple import config
+
+from ext.database import dbc
 
 
 class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='testando')
+    @commands.command(name='naquery')
     async def test(self, ctx):
-        await ctx.send('test')
+        conn = dbc(config("host"))
+        dump = conn.consult(config("naquery"))
+        for i in dump:
+            await ctx.send(i)
+
+    @commands.command(name='requery')
+    async def test(self, ctx):
+        conn = dbc(config("host"))
+        dump = conn.consult(config("requery"))
+        for i in dump:
+            await ctx.send(i)
 
 async def setup(bot):
     await bot.add_cog(test(bot))
