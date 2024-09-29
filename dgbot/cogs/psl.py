2d403a9f80a26f653a5a8b1593091ca3cde81365
douglasms90 <douglasms90@gmail.com>
2024-09-28 22:22:05 -0300

    psl.py

---
+++ dgbot/cogs/psl.py
@@ -0,0 +1,122 @@
+{"data":[
+    {
+        "carbohydrates":[
+            {
+                "id":11,
+                "nm":"arroz"
+            },
+            {
+                "id":12,
+                "nm":"feijão"
+            },
+            {
+                "id":13,
+                "nm":"macarrão"
+            },
+            {
+                "id":14,
+                "nm":"batata"
+            },
+            {
+                "id":15,
+                "nm":"pão"
+            },
+            {
+                "id":15,
+                "nm":"farinha"
+            },
+            {
+                "id":16,
+                "nm":"lentilha"
+            },
+            {
+                "id":17,
+                "nm":"quinoa"
+            },
+            {
+                "id":18,
+                "nm":"psyllium"
+            }
+        ]
+    },
+    {
+        "protein":[
+            {
+                "id":21,
+                "nm":"frango"
+            },
+            {
+                "id":22,
+                "nm":"ovo"
+            },
+            {
+                "id":23,
+                "nm":"carne bovina"
+            },
+            {
+                "id":24,
+                "nm":"peixe"
+            },
+            {
+                "id":22,
+                "nm":"carne suína"
+            },
+            {
+                "id":22,
+                "nm":"fígado bovino"
+            },
+
+        vegetables | Cebola
+        vegetables | Alho
+        vegetables | Abobrinha
+        vegetables | Pimentão
+        vegetables | Pepino
+        vegetables | Tomate
+        vegetables | Cenoura
+        vegetables | Cogumelos
+        vegetables | Brócolis
+        vegetables | Alface
+        vegetables | Abacate
+
+        drinks | Coca Cola
+        drinks | Café
+
+        fruits | Banana
+        fruits | Maçã
+        fruits | Limão
+        fruits | Morango
+
+        dairy | Leite
+        dairy | Queijo cottage
+        dairy | Requeijão
+        dairy | Creme de Leite
+        dairy | Iogurte natural
+
+        fats | Pasta de amendoim
+        fats | Linhaça
+        fats | Chia
+        fats | Óleo de linhaça
+        fats | Azeite
+        fats | Óleo de coco
+
+        spice | Manjericão
+        spice | Coentro
+        spice | Salsinha
+        spice | Alecrim
+        spice | Tomilho
+        spice | Orégano
+        spice | Páprica
+        spice | Pimenta
+        spice | Canela
+
+        HIGIENE | Sabonete
+        HIGIENE | Papel Higiênico
+        HIGIENE | Sabão em Pó
+
+        pantry | Chocolate amargo
+        pantry | Mel ou xarope de bordo
+        pantry | Gelatina zero
+        pantry | Vinagre balsâmico
+        pantry | Filtro de Café
+    }
+}
