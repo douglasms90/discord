c04314496b6efce3e78be3eb81a2d14bc201b91f
douglasms90 <douglasms90@gmail.com>
2024-11-16 22:10:01 -0300

    msc

--- dgbot/cogs/nails.py
+++ dgbot/cogs/nails.py
@@ -10,33 +10,83 @@ class nails(commands.Cog):
         self.bot = bot
 
     @commands.command(name="nails")
-    async def nails(self, ctx, *args):
-        now = datetime.now()
-        with databaseConnection(config("hostMydb")) as db:
-            db.insert("INSERT INTO nails(dt, pr) VALUES(%s, %s)", (now, args[0],))
-            view = db.read("SELECT * FROM nails WHERE dt=%s", (now,))
-        for i in view:
-            embed = discord.Embed(title=f'{now}',description='Unha')
-            embed.set_author(name=ctx.author)
-            embed.add_field(name='', value=f'', inline=True)
-            embed.set_footer(text=i[0])
-        await ctx.send(embed = embed)
+    async def nails(self, ctx):
+        if ctx.author.id in [269592803602989058, 825867087390048326]:  # D, D
+            view = discord.ui.View()
+            
+            button1 = discord.ui.Button(style=discord.ButtonStyle.secondary, label="20")
+            button2 = discord.ui.Button(style=discord.ButtonStyle.secondary, label="40")
+            button3 = discord.ui.Button(style=discord.ButtonStyle.secondary, label="120")
+            button4 = discord.ui.Button(style=discord.ButtonStyle.primary, label="Dia")
+            
+            async def um(interaction: discord.Interaction):
+                now = datetime.now()
+                with databaseConnection(config("hostMydb")) as db:
+                    db.insert("INSERT INTO nails(dt, pr) VALUES(%s, %s)", (now, 20,))
+                    view = db.read("SELECT * FROM nails WHERE dt=%s", (now,))
+                for i in view:
+                    embed = discord.Embed(title=f'{now}',description='Unha')
+                    embed.set_author(name=ctx.author)
+                    embed.add_field(name='', value=f'', inline=True)
+                    embed.set_footer(text=i[0])
+                await interaction.response.send_message(embed = embed, ephemeral=True)
+            
+            async def dois(interaction: discord.Interaction):
+                now = datetime.now()
+                with databaseConnection(config("hostMydb")) as db:
+                    db.insert("INSERT INTO nails(dt, pr) VALUES(%s, %s)", (now, 40,))
+                    view = db.read("SELECT * FROM nails WHERE dt=%s", (now,))
+                for i in view:
+                    embed = discord.Embed(title=f'{now}',description='Unha')
+                    embed.set_author(name=ctx.author)
+                    embed.add_field(name='', value=f'', inline=True)
+                    embed.set_footer(text=i[0])
+                await interaction.response.send_message(embed = embed, ephemeral=True)
+            
+            async def tres(interaction: discord.Interaction):
+                now = datetime.now()
+                with databaseConnection(config("hostMydb")) as db:
+                    db.insert("INSERT INTO nails(dt, pr) VALUES(%s, %s)", (now, 120,))
+                    view = db.read("SELECT * FROM nails WHERE dt=%s", (now,))
+                for i in view:
+                    embed = discord.Embed(title=f'{now}',description='Unha')
+                    embed.set_author(name=ctx.author)
+                    embed.add_field(name='', value=f'', inline=True)
+                    embed.set_footer(text=i[0])
+                await interaction.response.send_message(embed = embed, ephemeral=True)
+            
+            async def quatro(interaction: discord.Interaction):
+                dump = ""
+                with databaseConnection(config("hostMydb")) as db:
+                    today = db.read("SELECT * FROM nails WHERE DATE(dt) = CURRENT_DATE", (None))
+                for i in today:
+                    dump += f"{i[0]}, {i[1]}, {i[2]}\n"
+                await interaction.response.send_message(f"```{dump}```", ephemeral=True)
+            
+            button1.callback = um
+            button2.callback = dois
+            button3.callback = tres
+            button4.callback = quatro
+            
+            view.add_item(button1)
+            view.add_item(button2)
+            view.add_item(button3)
+            view.add_item(button4)
+            
+            await ctx.send("Qual o valor?", view=view)
+        else:
+            await ctx.send(f"{ctx.author} você não tem autorização.")
 
     @commands.command(name="nailsdelete")
     async def nailsdelete(self, ctx, *args):
-        with databaseConnection(config("hostMydb")) as db:
-            before = db.read("SELECT * FROM nails WHERE id = %s", (args[0],))
-            db.delete("DELETE FROM nails WHERE id = %s", (args[0],))
-        await ctx.send(f'{before}\nDeletado com sucesso.')
-
-    @commands.command(name="nailstoday")
-    async def nailstoday(self, ctx):
-        dump = ""
-        with databaseConnection(config("hostMydb")) as db:
-            today = db.read("SELECT * FROM nails WHERE DATE(dt) = CURRENT_DATE", (None))
-        for i in today:
-            dump += f"{i[0]}, {i[1]}, {i[2]}\n"
-        await ctx.send(f"```{dump}```")
+        if ctx.author.id in [269592803602989058, 825867087390048326]:  # D, D
+            with databaseConnection(config("hostMydb")) as db:
+                before = db.read("SELECT * FROM nails WHERE id = %s", (args[0],))
+                db.delete("DELETE FROM nails WHERE id = %s", (args[0],))
+            await ctx.send(f'{before}\nDeletado com sucesso.')
+        else:
+            await ctx.send(f"{ctx.author} você não tem autorização.")
 
 async def setup(bot):
     await bot.add_cog(nails(bot))
+
--- dgbot/ext/commands.py
+++ dgbot/ext/commands.py
@@ -1,43 +1,20 @@
 from ext.database import databaseConnection
 from decouple import config
 
-def dropdb():
-    base.metadata.drop_all(engine)
-
-def createdb():
-    base.metadata.create_all(engine)
-
-def insertdb():
-    with Session() as session:
-        #session.add(Act(id=1,dt=datetime.datetime(20023,1,17,18,2,0,0),usr='269592803602989058',ido='',sn='4D4B5047B4964348',ctr='65628',cto='M08'))
-        #session.add(Nail(id=5,dt=datetime.datetime(20024,9,14,11,0,0,0),pr=40))
-        #session.add(Mkt(id=100,mc='carb',nm='arroz 1',qt='5kg',kc=0,pu=29.90,pd=23.99)),
-        session.commit()
-
-def deletedb():
-    with Session() as session:
-        #session.query(Act).filter(Act.id == 100).delete()
-        session.commit()
-
-
-def updatedb():
-    with Session() as session:
-        #session.query(Act).filter(Act.id == 1).update(session.add(Mkt(mc='id':1)
-        #session.query(Nail).filter(Nail.id == 9).update(session.add(Mkt(mc='id':10)
-        #session.query(Nail).filter(Nail.id == 5).update(session.add(Mkt(mc='dt':datetime.datetime(2024,9,13,14,0,0,0))
-        session.commit()
 
 def create():
     with databaseConnection(config("hostAdmin")) as db:
-        db.create("CREATE DATABASE database")
+        #db.create("CREATE DATABASE database")
+        pass
 
 def createdt():
     with databaseConnection(config("hostMydb")) as db:
         "Tem que ser uma linha de cada vez"
-        db.createdt("CREATE TABLE mkt(id SERIAL PRIMARY KEY, mc VARCHAR(5), nm VARCHAR(25), pd FLOAT, pu FLOAT)")
-        db.createdt("CREATE TABLE atv(id SERIAL PRIMARY KEY, cl VARCHAR(20), nm VARCHAR(10), pr FLOAT, pm FLOAT, qt FLOAT, dv FLOAT, pl FLOAT, vp FLOAT)")
-        db.createdt("CREATE TABLE act(id SERIAL PRIMARY KEY, dt TIMESTAMP, us BIGINT, os INTEGER, sn VARCHAR(20), cr INTEGER, ct VARCHAR(5))")
-        db.createdt("CREATE TABLE nails(id SERIAL PRIMARY KEY, dt TIMESTAMP, pr FLOAT)")
+        #db.createdt("CREATE TABLE mkt(id SERIAL PRIMARY KEY, mc VARCHAR(5), nm VARCHAR(25), pd FLOAT, pu FLOAT)")
+        #db.createdt("CREATE TABLE atv(id SERIAL PRIMARY KEY, cl VARCHAR(20), nm VARCHAR(10), pr FLOAT, pm FLOAT, qt FLOAT, dv FLOAT, pl FLOAT, vp FLOAT)")
+        #db.createdt("CREATE TABLE act(id SERIAL PRIMARY KEY, dt TIMESTAMP, us BIGINT, os INTEGER, sn VARCHAR(20), cr INTEGER, ct VARCHAR(5))")
+        #db.createdt("CREATE TABLE nails(id SERIAL PRIMARY KEY, dt TIMESTAMP, pr FLOAT)")
+        pass
 
 def insert():
     with databaseConnection(config("hostMydb")) as db:
@@ -45,11 +22,14 @@ def insert():
         #db.insert("INSERT INTO atv (id, cl, nm, pr, pm, qt, dv, pl, vp) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)", (300, 'acoes', 'bbas3', 0, 0, 3.0, None, None, None))
         #db.insert("INSERT INTO act (id, dt, us, os, sn, cr, ct) VALUES(%s, %s, %s, %s, %s, %s, %s)", (1, '2023-01-17 18:02:00.000000', 269592803602989058, 201942, '4D4B5047B4964348', 65628, 'M08'))
         #db.insert("INSERT INTO nails (id, dt, pr) VALUES(%s, %s, %s)", (72, '2024-11-08 19:16:00.0', 20.0))
+        pass
 
 def update():
     with databaseConnection(config("hostMydb")) as db:
-        db.update("UPDATE datacamp_courses SET course_instructor = %s, topic = %s WHERE course_name = %s", ('New Instructor', 'New Topic', 'Introduction to SQL'))
+        #db.update("UPDATE datacamp_courses SET course_instructor = %s, topic = %s WHERE course_name = %s", ('New Instructor', 'New Topic', 'Introduction to SQL'))
+        pass
 
 def delete():
     with databaseConnection(config("hostMydb")) as db:
-        db.delete("DELETE FROM datacamp_courses WHERE topic = %s", ('Python',))
+        #db.delete("DELETE FROM datacamp_courses WHERE topic = %s", ('Python',))
+        pass
